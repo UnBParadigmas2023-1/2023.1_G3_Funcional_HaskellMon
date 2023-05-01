a24cc252e58031c6f9d141d627fe3d466befc767
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2023-05-01 19:02:43 -0300

    Escolha de ataque implementada na lógica de batalha
    Co-Authored-by: Mateus Moreira <mateuuusss@gmail.com>

--- app/main.hs
+++ app/main.hs
@@ -1,22 +1,23 @@
 import System.IO
 
 -- Definindo o tipo Pokemon
-data Pokemon = Pokemon { vida :: Int
+data Pokemon = Pokemon { nome :: String
+                       , vida :: Int
                        , ataque :: Int
                        } deriving (Show)
 
 -- Criando os pokémons
 pikachu :: Pokemon
-pikachu = Pokemon { vida = 100, ataque = 50 }
+pikachu = Pokemon { nome = "Pikachu", vida = 100, ataque = 50 }
 
 charmander :: Pokemon
-charmander = Pokemon { vida = 80, ataque = 70 }
+charmander = Pokemon { nome = "Charmander", vida = 80, ataque = 70 }
 
 squirtle :: Pokemon
-squirtle = Pokemon { vida = 120, ataque = 40 }
+squirtle = Pokemon { nome = "Squirtle", vida = 120, ataque = 40 }
 
 bulbasaur :: Pokemon
-bulbasaur = Pokemon { vida = 90, ataque = 60 }
+bulbasaur = Pokemon { nome = "Bulbasaur", vida = 90, ataque = 60 }
 
 -- Criando as listas de pokémons
 treinador :: [Pokemon]
@@ -45,23 +46,44 @@ escolherPokemon pokemons = do
       hFlush stdout
       escolherPokemon pokemons
 
+-- Função ataque
+funcaoAtaque :: Pokemon -> Pokemon -> Int -> IO Pokemon
+funcaoAtaque pokemonAtacante pokemonAlvo ataqueEscolhido
+    | ataqueEscolhido >= 1 && ataqueEscolhido <= 3 = do
+        let valorAtaque = ataque pokemonAtacante * ataqueEscolhido -- Random adicionar 
+        let novaVida = vida pokemonAlvo - valorAtaque
+        putStrLn $ nome pokemonAtacante ++ " inflingiu " ++ show valorAtaque ++ " de dano e deixou " ++ nome pokemonAlvo ++ " com " ++ show novaVida ++ " pontos de vida."
+        hFlush stdout
+        return Pokemon { nome = nome pokemonAlvo, vida = novaVida, ataque = ataque pokemonAlvo }
+    | otherwise = do
+        putStrLn "Opção inválida!"
+        hFlush stdout
+        funcaoAtaque pokemonAtacante pokemonAlvo ataqueEscolhido
+
+
 -- Função para simular uma batalha entre dois pokémons
 batalhar :: Pokemon -> Pokemon -> IO ()
 batalhar p1 p2
-  | vida p2 <= 0 = putStrLn $ show p2 ++ " foi derrotado!"
-  | otherwise = do
-      let dano = ataque p1
-      putStrLn $ show p1 ++ " atacou " ++ show p2 ++ " causando " ++ show dano ++ " de dano!"
-      hFlush stdout
-      let p2' = p2 { vida = vida p2 - dano }
-      batalhar p2' p1
+    | vida p1 <= 0 = putStrLn $ "Seu pokemon " ++ nome p1 ++ " foi derrotado!"
+    | vida p2 <= 0 = putStrLn $ "O pokemon " ++ nome p2 ++ " do ginásio foi derrotado!"
+    | otherwise = do
+        putStrLn "Opções de ataque: 1, 2, 3"
+        hFlush stdout
+
+        ataqueEscolhido <- getLine
+        let escolhaNum = read ataqueEscolhido
+
+        novoP2 <- funcaoAtaque p1 p2 escolhaNum
+        novoP1 <- funcaoAtaque p2 p1 1
+        batalhar novoP1 novoP2
+        
 
 -- Função principal
 main :: IO ()
 main = do
-  putStrLn "Bem-vindo ao jogo de Pokémon!"
-  hFlush stdout
-  pokemonEscolhido <- escolherPokemon treinador
-  putStrLn $ "Você escolheu o pokémon: " ++ show pokemonEscolhido
-  hFlush stdout
-  batalhar pokemonEscolhido (head ginasio)
+    putStrLn "Bem-vindo ao jogo de Pokémon!"
+    hFlush stdout
+    pokemonEscolhido <- escolherPokemon treinador
+    putStrLn $ "Você escolheu o pokémon: " ++ show pokemonEscolhido
+    hFlush stdout
+    batalhar pokemonEscolhido (head ginasio)
