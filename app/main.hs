a925f7f7198d0fd4dc18faaa4c285d26904a2d2a
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-05-02 14:14:51 -0300

    Adiciona verificação de input.

--- app/main.hs
+++ app/main.hs
@@ -1,7 +1,14 @@
 import Hw
 import Pre_batle
 
+batle_result 
+
 main = 
     do
         hw
-        pre_batle
+        -- pre_batle   
+        aux<-pre_batle
+        print (aux)      
+
+
+-- teste =  let pre_batle
--- app/pre_batle.hs
+++ app/pre_batle.hs
@@ -2,12 +2,53 @@ module Pre_batle where
 
 import System.Process
 
-list_gym =
+
+verificaEntrada :: Int -> Int
+verificaEntrada x
+                |x==1=1
+                |x==2=2
+                |x==3=3
+                |otherwise = 0;
+
+list_gym :: IO Int
+list_gym  = 
     do
         system "clear"
         putStrLn "\t\tGinásios\n\n"
         putStrLn "[1] Ginásio foo\n\tDificuldade: Fácil\n"
         putStrLn "[2] Ginásio bar\n\tDificuldade: Médio\n"
         putStrLn "[3] Ginásio xpto\n\tDificuldade: Dificil\n"
+        input <- readLn :: IO Int
+        -- print (verificaEntrada input)
+        return (verificaEntrada input)
+        -- return input
+
+
+
+
 
 pre_batle = list_gym
+
+
+
+
+
+
+
+-- list_gym :: IO Int
+-- list_gym  = 
+--     do
+--         system "clear"
+--         putStrLn "\t\tGinásios\n\n"
+--         putStrLn "[1] Ginásio foo\n\tDificuldade: Fácil\n"
+--         putStrLn "[2] Ginásio bar\n\tDificuldade: Médio\n"
+--         putStrLn "[3] Ginásio xpto\n\tDificuldade: Dificil\n"
+--         input <- readLn :: IO Int
+--         case input of
+--             1 -> return input
+--             2 -> return input
+--             3 -> return input
+--             _ -> do 
+--                     system "clear"
+--                     putStrLn "Digite um valor inteiro valido (1, 2 ou 3)"
+--                     list_gym
