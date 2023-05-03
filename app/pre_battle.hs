2f64c74cbea89f87b6a27a6f96c692687c072775
Daniel Porto <danielportods@gmail.com>
2023-05-03 19:07:16 -0300

    Simplifica a chamada do menu em recursão

--- app/pre_battle.hs
+++ app/pre_battle.hs
@@ -1,39 +1,31 @@
 module Pre_battle where
 import System.Process
+import System.IO
+import Control.Concurrent
 
+list_gym :: String -> IO Int
 
-verificaEntrada :: Int -> Int
-verificaEntrada x
-                |x==1=1
-                |x==2=2
-                |x==3=3
-                |otherwise = 0;
-
-le_input :: IO Int
-le_input=
-    do
-        input <- readLn :: IO Int
-        result <- return (verificaEntrada input)
-        if result == 0
-            then do
-                putStrLn "Opção inválida. Por favor, tente novamente."
-                le_input 
-            else
-                return result
-
-list_gym :: IO Int
-list_gym  = 
+list_gym "" = 
     do
         system "clear"
-        putStrLn "\t\tGinásios\n\n"
+        putStrLn "[x] Quit\tGinásios\n"
         putStrLn "[1] Ginásio foo\n\tDificuldade: Fácil\n"
         putStrLn "[2] Ginásio bar\n\tDificuldade: Médio\n"
-        putStrLn "[3] Ginásio xpto\n\tDificuldade: Dificil\n"
-        ginasio<-le_input
+        putStrLn "[3] Ginásio xpto\n\tDificuldade: Dificil" 
+        
+        putStr "\n>> "
+        hFlush stdout 
+        input <- getLine
+        list_gym input
 
-        return ginasio
+list_gym i
+    | i == "x" || i == "X" = return 0
+    | i == "1" = return 1
+    | i == "2" = return 2
+    | i == "3" = return 3
+    | otherwise = do
+        putStrLn "Opção inválida. Por favor, tente novamente."
+        threadDelay 2500000
+        list_gym "" 
 
-
-
-pre_battle = list_gym
-
+pre_battle = list_gym ""
