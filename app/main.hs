4fab6865332e0ca2c30023e8bf0034d093656278
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2023-05-03 13:44:39 -0300

    Adicionando versão inicial do menu
    Essa versão ainda não está completa
    
    Co-Authored-By: Vitor Diniz Pagani Vieira Ribeiro <vitordiniz25@gmail.com>

--- app/main.hs
+++ app/main.hs
@@ -1,3 +1,24 @@
 import Hw
+import System.Exit (exitSuccess)
+import System.IO
 
-main = hw
+main = do
+    putStrLn "Welcome to HaskellMon"
+    menu
+
+menu = do 
+    putStrLn "1. Start"
+    putStrLn "2. Quit"
+    putStr "Please choose an option: "
+    hFlush stdout -- limpa o buffer de saída
+    choice <- getLine -- lê a escolha do usuário
+    case choice of
+        "1" -> do
+            putStrLn "Starting HaskellMon..."
+            -- aqui você pode colocar o código que inicia o jogo
+        "2" -> do
+            putStrLn "Goodbye!"
+            exitSuccess
+        _   -> do
+            putStrLn "Invalid choice. Please choose again."
+            menu
