c79b72cc95f21669e68b81ef6f8483751d6719a2
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2023-05-03 19:52:24 -0300

    Adicionando mais elementos ao menu principal
    Co-Authored-By: Kess Jhones <49600484+kessJhones@users.noreply.github.com>

--- app/main.hs
+++ app/main.hs
@@ -1,24 +1,38 @@
 import Hw
 import System.Exit (exitSuccess)
-import System.IO
+import System.IO ( stdout, hFlush )
+import System.Process
 
+main :: IO ()
 main = do
-    putStrLn "Welcome to HaskellMon"
+    
     menu
 
+
+clearScreen :: IO ()
+clearScreen = system "cls" >> return ()
+
+menu :: IO ()
 menu = do 
-    putStrLn "1. Start"
-    putStrLn "2. Quit"
-    putStr "Please choose an option: "
+    
+    putStrLn "=========================================="
+    putStrLn "\tBem-vindo(a) ao HaskellMon"
+    putStrLn "=========================================="
+    putStrLn " "
+    putStrLn "1. Iniciar"
+    putStrLn "2. Sair"
+    putStr "Escolha uma opcao: "
+    
     hFlush stdout -- limpa o buffer de saída
     choice <- getLine -- lê a escolha do usuário
     case choice of
         "1" -> do
-            putStrLn "Starting HaskellMon..."
-            -- aqui você pode colocar o código que inicia o jogo
+            putStrLn "Iniciando HaskellMon..."
+            
         "2" -> do
-            putStrLn "Goodbye!"
+            putStrLn "Tchau!"
             exitSuccess
         _   -> do
-            putStrLn "Invalid choice. Please choose again."
+            clearScreen
+            putStrLn "Escolha inválida, digite novamente"
             menu
